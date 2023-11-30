be760e77a1efcc70a75cc4862ce1c40b35c36114
vafo <v.safokhujaev@student.inha.uz>
2023-11-30 16:33:23 +0500

    mutex init
    Related to #3

---
+++ src/mutex/mutex.hpp
@@ -0,0 +1,14 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+
+namespace concurrency {
+
+class mutex {
+
+
+
+}; // class mutex
+
+} // namespace concurrency
+
+#endif
---
+++ test/mutex_test.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch_all.hpp>
+
+#include "thread.hpp"
+#include "mutex.hpp"
+
+namespace concurrency {
+
+TEST_CASE("mutex: creation and deletion", "[mutex]") {
+    
+}
+
+} // namespace concurrency
