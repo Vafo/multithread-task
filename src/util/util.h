89232f8e40d2f9df9af284aba7e12043a58803d2
vafo <v.safokhujaev@student.inha.uz>
2023-12-19 17:45:03 +0500

    remake of c retval wrapper
    There is a clear need to optimize it by compile time as much as possible
    as it weights just too much (function & string members are redundantly heavy)
    
    Related to #10

--- src/util/util.h
+++ src/util/util.h
@@ -8,38 +8,112 @@
 
 namespace concurrency::util {
 
-template<typename func_retval_T>
-class try_call {
+struct no_auto_throw_t {};
 
+constexpr no_auto_throw_t no_auto_throw;
+
+template<typename func_retval_T>
+struct cerror_code_base {
+protected:
+    template<
+        typename Callable,
+        typename err_prefix_T
+    >
+    cerror_code_base(
+        err_prefix_T err_prefix,
+        Callable err_msg_gen,
+        func_retval_T valid_retval,
+        bool is_auto_throw
+    )
+        : m_err_msg_gen(err_msg_gen)
+        , m_err_prefix(err_prefix)
+        , m_valid_retval(valid_retval)
+
+        , m_auto_throw(is_auto_throw)
+        , m_is_valid(true)
+    { }
+
+public:
+    void operator=(
+        func_retval_T retval
+    ) {
+        m_is_valid = retval == m_valid_retval;
+
+        if(m_auto_throw && !valid()) {
+            throw_exception();
+        }
+    }
+
+    void throw_exception() {
+        std::string err_msg = m_err_msg_gen(m_err_prefix, retval);
+        throw std::runtime_error(err_msg);
+    }
+
+    bool valid() const
+    { return m_is_valid; }
+
+    operator bool() const
+    { return !valid(); }
+
+private:
+    typename func::function< 
+        std::string(std::string, func_retval_T)
+    > m_err_msg_gen;
+    std::string m_err_prefix;
+    func_retval_T m_valid_retval;
+    bool m_auto_throw;
+    bool m_is_valid;
+};
+
+template<typename func_retval_T>
+class cerror_code: public cerror_code_base<func_retval_T> {
+    typedef cerror_code_base<func_retval_T> base_class;
 public:
     template<
         typename Callable,
         typename err_prefix_T
     >
-    try_call(
+    cerror_code(
         err_prefix_T err_prefix,
-        Callable err_msg_gen
-    ):
-        m_err_msg_gen(err_msg_gen),
-        m_err_prefix(err_prefix)
-    {}
+        Callable err_msg_gen,
+        func_retval_T valid_retval
+    )
+        : base_class (
+            err_prefix,
+            err_msg_gen,
+            valid_retval,
+            false
+        )
+    { }
 
-    func_retval_T operator()(
-        func_retval_T retval,
-        func_retval_T valid_val
+    template<
+        typename Callable,
+        typename err_prefix_T
+    >
+    cerror_code(
+        err_prefix_T err_prefix,
+        Callable err_msg_gen,
+        func_retval_T valid_retval,
+        no_auto_throw_t
+    )
+        : base_class (
+            err_prefix,
+            err_msg_gen,
+            valid_retval,
+            true
+        )
+    { }
+
+    void operator=(
+        func_retval_T retval
     ) {
-        if(retval != valid_val) {
-            std::string err_msg = m_err_msg_gen(m_err_prefix, retval);
-            throw std::runtime_error(err_msg);
-        }
-
-        return retval;
+        base_class::operator=(retval);
     }
 
-private:
-    typename func::function< std::string(std::string, func_retval_T) > m_err_msg_gen;
-    std::string m_err_prefix;
-}; // class try_call
+    operator bool() const
+    { return base_class::operator bool(); }
+
+}; // class cerror_code
 
 } // namespace concurrency::util
 
