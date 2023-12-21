522928c657f727c29780615fad6212dd61620bdb
vafo <v.safokhujaev@student.inha.uz>
2023-12-21 17:53:54 +0500

    minor braces & funcs reorder
    Related to #10

--- src/thread/thread.hpp
+++ src/thread/thread.hpp
@@ -12,14 +12,13 @@ public:
     typedef pthread_t native_handle_type;
 
     thread()
-        : m_thread_id()
+    : m_thread_id()
     { }
     
     template<typename Callable, typename ...Args>
     explicit
     thread(Callable callb, Args ...args) 
-        : m_thread_id()
-    {
+    : m_thread_id() {
         // arguments must be invocable after conversion to rvalues (no lvalue references)
         auto lambda_func = [=] () -> void { callb(args...); };
 
@@ -32,6 +31,12 @@ public:
     thread(thread&& other)
     { swap(other); };
 
+    ~thread() noexcept(false) /*TODO: find alternative to suit >c++11*/ {
+        if(joinable())
+            /*since c++11 throw in destructor causes terminate*/
+            throw std::runtime_error("thread: ~thread: thread was not joined");
+    }
+
     thread& operator=(const thread& other) = delete;
     
     thread& operator=(thread&& other) {
@@ -39,17 +44,6 @@ public:
             throw std::runtime_error("thread: operator=: thread was not joined");
         swap(other);
         return *this;
-    }
-    
-    ~thread() noexcept(false) /*TODO: find alternative to suit >c++11*/ {
-        if(joinable())
-            /*since c++11 throw in destructor causes terminate*/
-            throw std::runtime_error("thread: ~thread: thread was not joined");
-    }
-
-    void swap(thread& other) {
-        using std::swap;
-        swap(m_thread_id, other.m_thread_id);
     }
 
     bool joinable();
@@ -59,13 +53,16 @@ public:
     void detach();
 
     native_handle_type&
-    get_id() {
-        return m_thread_id;
-    }
+    get_id()
+    { return m_thread_id; }
 
     const native_handle_type&
-    get_id() const {
-        return m_thread_id;
+    get_id() const
+    { return m_thread_id; }
+    
+    void swap(thread& other) {
+        using std::swap;
+        swap(m_thread_id, other.m_thread_id);
     }
 
     friend void swap(thread& lhs, thread& rhs)
@@ -83,25 +80,19 @@ private:
 class jthread {
 
 public:
-    jthread(): m_thread() {}
+    jthread()
+    : m_thread()
+    {}
 
     template<typename Callable, typename ...Args>
     jthread(Callable cb, Args ...args)
-        : m_thread(cb, args...)
+    : m_thread(cb, args...)
     { }
 
-    jthread(jthread&& other): m_thread() {
-        swap(other);
-    }
+    jthread(jthread&& other): m_thread()
+    { swap(other); }
 
     jthread(const jthread& other) = delete;
-
-    jthread& operator=(const jthread& other) = delete;
-    
-    jthread& operator=(jthread&& other) {
-        jthread(std::move(other)).swap(*this);
-        return *this;
-    };
 
     ~jthread() {
         if(m_thread.joinable()) {
@@ -109,10 +100,12 @@ public:
         }
     }
 
-    void swap(jthread& other) { 
-        using std::swap;
-        swap(m_thread, other.m_thread);
-    }
+    jthread& operator=(const jthread& other) = delete;
+    
+    jthread& operator=(jthread&& other) {
+        jthread(std::move(other)).swap(*this); // move to tmp & swap
+        return *this;
+    };
 
     bool joinable()
     { return m_thread.joinable(); }
@@ -124,13 +117,16 @@ public:
     { m_thread.detach(); }
 
     pthread_t&
-    get_id() {
-        return m_thread.get_id();
-    }
+    get_id()
+    { return m_thread.get_id(); }
 
     const pthread_t&
-    get_id() const {
-        return m_thread.get_id();
+    get_id() const
+    { return m_thread.get_id(); }
+
+    void swap(jthread& other) { 
+        using std::swap;
+        swap(m_thread, other.m_thread);
     }
 
     friend void swap(jthread& lhs, jthread& rhs)
