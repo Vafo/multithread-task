aed4e37999d557f5938d9ec677a6f7695c85ba1b
vafo <v.safokhujaev@student.inha.uz>
2023-12-29 17:51:10 +0500

    (j)thread interface deduplication
    Related to #10

--- src/thread/jthread.hpp
+++ src/thread/jthread.hpp
@@ -5,25 +5,25 @@
 
 namespace concurrency {
 
-class jthread {
-private:
-    thread m_thread;
+class jthread: public thread {
+// private:
+//     thread m_thread;
 
-private:
-    jthread(const jthread& other) = delete;
-    jthread& operator=(const jthread& other) = delete;
+// private:
+//     jthread(const jthread& other) = delete;
+//     jthread& operator=(const jthread& other) = delete;
 
 public:
     jthread()
-    : m_thread()
+        : thread()
     {}
 
     template<typename Callable, typename ...Args>
     jthread(Callable cb, Args ...args)
-    : m_thread(cb, args...)
-    { }
+        : thread(cb, args...)
+    {}
 
-    jthread(jthread&& other): m_thread()
+    jthread(jthread&& other)
     { swap(other); }
 
     jthread& operator=(jthread&& other) {
@@ -32,34 +32,36 @@ public:
     };
 
     ~jthread() {
-        if(m_thread.joinable()) {
-            m_thread.join();
+        if(joinable()) {
+            join();
         }
     }
 
-public:
-    void join()
-    { m_thread.join(); }
+// public:
+//     void join()
+//     { m_thread.join(); }
 
-    void detach()
-    { m_thread.detach(); }
+//     void detach()
+//     { m_thread.detach(); }
 
-    bool joinable()
-    { return m_thread.joinable(); }
+//     bool joinable()
+//     { return m_thread.joinable(); }
 
-public:
-    pthread_t&
-    get_id()
-    { return m_thread.get_id(); }
+// public:
+//     pthread_t&
+//     get_id()
+//     { return m_thread.get_id(); }
 
-    const pthread_t&
-    get_id() const
-    { return m_thread.get_id(); }
+//     const pthread_t&
+//     get_id() const
+//     { return m_thread.get_id(); }
 
 public:
     void swap(jthread& other) { 
         using std::swap;
-        swap(m_thread, other.m_thread);
+
+        thread::swap(other);
+        // swap(m_thread, other.m_thread);
     }
 
     friend void swap(jthread& lhs, jthread& rhs)
